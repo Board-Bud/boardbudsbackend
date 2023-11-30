2cff797b04fb395ad8e28a512a0be779009239c7
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-12-01 10:20:19 +1100

    feat: surfbaord crud

--- src/controllers/SurfboardController.js
+++ src/controllers/SurfboardController.js
@@ -6,8 +6,88 @@ router.get("/all", async (request, response) => {
     let result = await Surfboard.find({});
 
     response.json({
-        surfbaords: result
+        surfboards: result
     });
 });
+
+// Find one surfboard by its ID
+router.get("/one/id/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+// Find one surfbaord by its name 
+// localhost:3000/surfbaords/one/name/MickFanningBeastie
+router.get("/multiple/name/:nameToSearchFor", async (request, response) => {
+	let result = null;
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+router.get("/multiple/type/:typeToFilterBy", async (request, response) => {
+	let result = null;
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+
+// Create a new surfbaord in the DB
+// POST localhost:3000/cats/
+router.post("/", async (request, response) => {
+
+	// Error handling via try-catch
+	// let result = null;
+	// try {
+	// 	result = await Surfboard.create(request.body);
+	// } catch (error) {
+	// 	result = error;
+	// }
+
+	// Error handling via Promise.catch()
+	let result = await Surfboard.create(request.body).catch(error => {return error});
+	
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+// Update an existing surfboard in the DB.
+// Find one surfboard by its ID, and modify that surfboard. 
+// Patch is for whatever properties are provided,
+// does not overwrite or remove any unmentioned properties of the surfboard
+router.patch("/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+// Find one surfboard by its ID,
+// and delete it from the DB.
+router.delete("/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		surfboard: result
+	});
+
+});
+
+
+
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+
+
 // make a server instance
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
     response.json({
