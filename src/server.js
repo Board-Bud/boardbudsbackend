d05bb63f20dd5459660b110c34d2318c2f765ecd
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-12-06 09:45:44 +1100

    feat: Procfile

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ const express = require('express');
 // make a server instance
 const app = express();
 
+// const cors = require('cors');
+// const corsOptions = {
+// 	//			frontend localhost,  frontend deployed
+// 	origin: ["http://localhost:3000/","http://localhost:3000", "https://someDeployedWebsite.com"],
+// 	optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get("/", (request, response) => {
