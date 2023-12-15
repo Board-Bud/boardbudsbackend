e51faa92297bc37f42cb25839490495946ba3740
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-12-15 16:22:54 +1100

    fix: change auth routes to post, from get

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,8 +4,8 @@ const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
 // REGISTER/CREATE
-router.get("/register", register);
-router.get("/login", login);
+router.post("/register", register);
+router.post("/login", login);
 
 
 module.exports = router;
