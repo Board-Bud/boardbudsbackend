448e029a8bcbf71d7ecbd159cdb5c8fb22daeaac
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-12-15 11:22:28 +1100

    Update database/server connection code
    simplify the code so I can understand easier

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
 require('dotenv').config();
-
-const { databaseConnect } = require('./database');
-const { app } = require('./server');
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, async () => {
-	await databaseConnect();
-	console.log("Server running!");
-});
-
+const express = require('express');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
 const SurfboardRouter = require('./routes/surfboards');
-app.use('/surfboards', SurfboardRouter);
-
 const UserRouter = require('./routes/user');
-app.use("/users", UserRouter);
-
 const AuthRouter = require('./routes/auth');
+const cors = require('cors');
 
+const app = express();
+
+const connect = async () => {
+	try {
+	  await mongoose.connect(process.env.DB_URI);
+	  console.log("Connected to mongoDB.");
+	} catch (error) {
+	  throw error;
+	}
+  };
+  
+  mongoose.connection.on("disconnected", () => {
+	console.log("mongoDB disconnected!");
+  });
+
+
+app.use(cors())
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/surfboards', SurfboardRouter);
+app.use("/users", UserRouter);
 app.use("/auth", AuthRouter);
-
-
 
 
 // Error Handling Middleware
@@ -38,3 +43,8 @@ app.use((err,req,res,next)=>{
 		stack: err.stack
 	});
 });
+
+app.listen(3000, () => {
+	connect();
+	console.log("Connected to backend.");
+  });
