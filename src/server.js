572f11afe9986b38402578f9a0e0eda67a05b5c1
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-12-17 11:08:29 +1100

    feat: update cors

--- src/server.js
+++ src/server.js
@@ -11,9 +11,13 @@ const corsOptions = {
 	//			frontend localhost,  frontend deployed
 	origin: ["http://localhost:3000/","https://localhost:3000", "http://localhost:3001/","https://localhost:3001", "https://board-bud-c9fcbefcb666.herokuapp.com/"],
 	optionsSuccessStatus: 200,
-
-}
+    credentials: true,
+    preflightContinue: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS']
+};
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 
 
